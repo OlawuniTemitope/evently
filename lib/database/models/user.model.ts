0cb1d80b8351a37a3444048b2f75c7253f545817
Temitope Olawuni <olawunitemitope68@gmail.com>
2024-07-07 17:15:35 +0100

    corrected user data

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,14 +1,33 @@
 import { Schema, model, models } from "mongoose";
 
 const UserSchema = new Schema({
-  clerkId: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  username: { type: String, required: true, unique: true },
-  firstName: { type: String },
-  lastName: {type: String },
-  photo: { type: String},
+  clerkId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
 })
 
-const User = models.User || model('User', UserSchema);
+const User = models?.User || model('User', UserSchema);
 
 export default User;
